5c7f1235165166bda4ed4b32e65a00fa01e8827f
darkrecher <r@recher.recher>
2026-03-15 23:09:58 +0100

    Ajout d'une copie de puzzlescript dans une url presque-secrète.

---
+++ TUR-ROX-YZ/puzzlescript/games_dat.js
@@ -0,0 +1,10 @@
+//do not change go const...is modified elsewhere
+let games_gallery = [
+  {
+    "url": "https://www.puzzlescript.net/",
+    "thumb": "none.gif",
+    "title": "Sorry, I decided to remove the sample games",
+    "author": "Recher removed everything",
+    "homepage": "https://www.puzzlescript.net"
+  }
+]
